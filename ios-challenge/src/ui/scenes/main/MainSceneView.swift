0df8f39ed0381e7efdd8203752683ca1ff73b2ee
Marc Flores <mrk@MacBook-Pro-de-Marc.local>
2024-04-10 11:04:38 +0200

    initial commit

---
+++ ios-challenge/src/ui/scenes/main/MainSceneView.swift
@@ -0,0 +1,18 @@
+//
+//  MainSceneView.swift
+//  ios-challenge
+//
+//  Created by Marc Flores on 10/4/24.
+//
+
+import SwiftUI
+
+struct MainSceneView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainSceneView()
+}
